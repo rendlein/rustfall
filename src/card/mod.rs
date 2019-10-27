6c626344c92575e94cf783b916e2d97fcc8146d9
Jacob Boone <jboone989@gmail.com>
2019-10-27 16:07:44 -0400

    Initial Commit

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -2,8 +2,29 @@
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
+pub struct CardList {
+
+}
+
+#[derive(Deserialize, Debug)]
 pub struct Card {
     name: String,
-    type_line: String,
-    mana_cost: String
+    mana_cost: Option<String>,
+    oracle_text: Option<String>,
+    power: Option<String>,
+    toughness: Option<String>,
+    type_line: String
+}
+
+impl Card {
+    pub fn new() -> Self {
+        Card {
+            name: "".to_string(),
+            mana_cost: None,
+            oracle_text: None,
+            power: None,
+            toughness: None,
+            type_line: "".to_string()
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
 mod card;
 
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod tests;
